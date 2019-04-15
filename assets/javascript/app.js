6963a15478b91b7fc99ee1bbe4237f90dff7228b
Mohini Modi <Mohini.Modi@resmed.com>
2019-04-15 00:45:20 -0700

    database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,12 +22,14 @@
                 let destination = $("#destination-input").val().trim()
                 let time = $("#time-input").val().trim()
                 let frequency = $("#frequency-input").val().trim()
+                console.log(train_name);
+                console.log(frequency);
 
                 database.ref().push({
                     train: train_name,
-                    destination: destination,
-                    time: time,
-                    frequency: frequency
+                    destination1: destination,
+                    time1: time,
+                    frequency1: frequency
 
                 });
 
@@ -51,9 +53,9 @@
             database.ref().orderByChild("dateAdded").limitToLast(1).on('child_added',function(snapshot){    
             buildRow(snapshot);
             $("#train_name-display").text(snapshot.val().train);
-            $("#destination-display").text(snapshot.val().destination);
-            $("#time-display").text(snapshot.val().time);
-            $("#frequency-display").text(snapshot.val().frequency);
+            $("#destination-display").text(snapshot.val().destination1);
+            $("#time-display").text(snapshot.val().time1);
+            $("#frequency-display").text(snapshot.val().frequency1);
             });
 
 
@@ -68,23 +70,23 @@
                 newRow.append(trainCol);
 
                 var roleCol = $("<td>");
-                roleCol.text(snapshot.val().destination);
+                roleCol.text(snapshot.val().destination1);
                 newRow.append(roleCol);
 
                 var sdCol = $("<td>");
-                sdCol.text(snapshot.val().time);
+                sdCol.text(snapshot.val().time1);
                 newRow.append(sdCol);
 
 
                 var salCol = $("<td>");
-                salCol.text(snapshot.val().frequency);
+                salCol.text(snapshot.val().frequency1);
                 newRow.append(salCol);
 
                 newRow.appendTo("#empTable");
                 console.log(snapshot.val().train);
-            console.log(snapshot.val().destination);
-            console.log(snapshot.val().time);
-            console.log(snapshot.val().frequency);
+            console.log(snapshot.val().destination1);
+            console.log(snapshot.val().time1);
+            console.log(snapshot.val().frequency1);
             }
         });
  
